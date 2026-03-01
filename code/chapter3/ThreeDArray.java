b268084cf3eb452875bb571cb64f001b8e48f9aa
Znoywho <znoywho1508@gmail.com>
2026-03-01 11:50:14 +0700

    Check

--- code/chapter3/ThreeDArray.java
+++ code/chapter3/ThreeDArray.java
@@ -1,13 +1,13 @@
-public class ThreeDArray {
-  public static void main(String[] args) {
-    double  array = {
-      {0*2,0*3,0*5,0*6},
-      {0*2,0*8,0*9,0*7},
-    }
-    
-    
-
-
-
-  }
-}
+// public class ThreeDArray {
+// public static void main(String[] args) {
+// double array = {
+// {0*2,0*3,0*5,0*6},
+// {0*2,0*8,0*9,0*7},
+// }
+//
+//
+//
+//
+//
+// }
+// }
